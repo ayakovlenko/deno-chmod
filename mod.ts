9156675f4f7a32675384f60fb6c860e2ff535aae
tony <ayakovlenko@users.noreply.github.com>
2024-10-09 23:16:57 +0300

    export types

--- mod.ts
+++ mod.ts
@@ -1,3 +1,5 @@
 export { parseSymbolic } from "./parse.ts";
 
 export { decimal2octal, octal2decimal, symbolic2octal } from "./convert.ts";
+
+export type { PermissionGroup, PermissionGroups } from "./types.ts";
--- types.ts
+++ types.ts
@@ -1,9 +1,15 @@
+/**
+ * An object that holds owner, group, and other permissions.
+ */
 export interface PermissionGroups {
   owner: PermissionGroup; // u
   group: PermissionGroup; // g
   other: PermissionGroup; // o
 }
 
+/**
+ * An object that holds read, write, and execute permission flags.
+ */
 export interface PermissionGroup {
   read: boolean; // r
   write: boolean; // w
