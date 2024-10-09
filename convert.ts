4b9c1b51ef0cfb7cf44c8c42bfc8de958dc047fa
tony <ayakovlenko@users.noreply.github.com>
2024-10-09 23:33:41 +0300

    document converter functions

--- convert.ts
+++ convert.ts
@@ -2,16 +2,50 @@ import { parseSymbolic } from "./parse.ts";
 
 import type { PermissionGroup } from "./types.ts";
 
+/**
+ * Converts a symbolic representation of Unix permissions into an octal representation.
+ *
+ * ```typescript
+ * import { assertEquals } from "@std/assert";
+ *
+ * assertEquals(symbolic2octal("-rw-r--r--"), 644);
+ *
+ * assertEquals(symbolic2octal("-rwxr-xr-x"), 755);
+ *
+ * assertEquals(symbolic2octal("drwxr-xr-x"), 755);
+ * ```
+ */
 function symbolic2octal(expr: string): number {
   const { owner, group, other } = parseSymbolic(expr);
   return octal(owner) * 100 + octal(group) * 10 + octal(other);
 }
 
-// the decimal format is used by Deno.stat
+/**
+ * Converts an octal representation of Unix permissions into a decimal representation.
+ *
+ * Useful for setting permissions with {@link Deno.chmod}.
+ *
+ * ```typescript
+ * import { assertEquals } from "@std/assert";
+ *
+ * assertEquals(octal2decimal(777), 33279);
+ * ```
+ */
 function octal2decimal(p: number): number {
   return Number.parseInt((100_000 + p).toString(), 8);
 }
 
+/**
+ * Converts a decimal representation of Unix permissions into an octal representation.
+ *
+ * Useful for converting `mode` returned by {@link Deno.stat}.
+ *
+ * ```typescript
+ * import { assertEquals } from "@std/assert";
+ *
+ * assertEquals(decimal2octal(33279), 777);
+ * ```
+ */
 function decimal2octal(p: number): number {
   return parseInt((p - 0o100_000).toString(8));
 }
